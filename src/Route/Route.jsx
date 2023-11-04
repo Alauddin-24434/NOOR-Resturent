5fa19ade589d4a7cf0975e0a25ef6514afb75411
Alauddin-24434 <muhammadalauddin24434@gmail.com>
2023-11-04 22:45:03 +0600

    besic setup

---
+++ src/Route/Route.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+const myRoute=createBrowserRouter([
+    {
+        path:'/',
+        element: <h2>This is Main</h2>
+    }
+])
+
+export default myRoute
